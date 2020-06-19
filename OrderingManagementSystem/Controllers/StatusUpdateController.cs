31df9fc31c1eb82fde709db502d6fc3cb0113eee
ayaka horii <ayaka.h.626@gmail.com>
2020-06-19 13:32:41 +0900

    StatusUpdateControllerを修正。

---
+++ OrderingManagementSystem/Controllers/StatusUpdateController.cs
@@ -0,0 +1,34 @@
+﻿using OrderingManagementSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderingManagementSystem.Controllers
+{
+    public class StatusUpdateController : Controller
+    {
+        // GET: StatusUpdate
+        public ActionResult OrderStatusUpdate(int detailNo)
+        {
+            ModelContext db = new ModelContext();
+            ViewBag.model = db.OrderDetails.Find(detailNo);
+
+            return View();
+        }
+
+        public ActionResult OrderStatusChange(int detailNo, string status)
+        {
+            using(var db = new ModelContext())
+            {
+                var od = db.OrderDetails.Find(detailNo);
+                od.Status = status;
+                db.SaveChanges();
+
+                ViewBag.model = db.OrderDetails.Find(detailNo);
+            }
+            return View();
+        }
+    }
+}
