edccb49393dbe90121e7bd5a7590561bfe447b63
Xue Jiayi <sxbjxjy@hotmail.com>
2020-06-19 13:06:05 +0900

    担当者マスタ機能

---
+++ OrderingManagementSystem/ViewModel/EmployeeListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using OrderingManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrderingManagementSystem.ViewModel
+{
+    [CustomValidation(typeof(EmployeeListViewModel), "NoChecked")]
+    public class EmployeeListViewModel
+    {
+        [DisplayName("社員番号")]
+        public int EmpNo { get; set; }
+
+        [DisplayName("担当者氏名")]
+
+        public string EmpName { get; set; }
+
+        public Boolean IsChecked { get; set; }
+
+
+        public EmployeeListViewModel() { }
+
+        public EmployeeListViewModel(Employee emp)
+        {
+            EmpNo = emp.EmpNo;
+            EmpName = emp.EmpName;
+        }
+
+
+    }
+}
