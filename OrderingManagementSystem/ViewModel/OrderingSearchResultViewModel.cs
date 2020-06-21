fbbfb6c21f9f30b3ee4147633e5641d5872239a8
ayaka horii <ayaka.h.626@gmail.com>
2020-06-22 01:04:10 +0900

    バリデーションうまくいかない。

--- OrderingManagementSystem/ViewModel/OrderingSearchResultViewModel.cs
+++ OrderingManagementSystem/ViewModel/OrderingSearchResultViewModel.cs
@@ -11,17 +11,17 @@ namespace OrderingManagementSystem.ViewModel
     public class OrderingSearchResultViewModel
     {
         [DisplayName("顧客ID")]
-        [DisplayFormat(DataFormatString = "0:d6")]
+        [DisplayFormat(DataFormatString = "{0:d6}")]
         [StringLength(6, ErrorMessage = "{0}は{1}桁以内で入力してください")]
         [Range(0, 999999, ErrorMessage = "{0}は数値で入力してください")]
         public int CustomerId { get; set; }
         [DisplayName("注文番号")]
-        [DisplayFormat(DataFormatString = "0:d6")]
+        [DisplayFormat(DataFormatString = "{0:d6}")]
         [StringLength(6, ErrorMessage = "{0}は{1}桁以内で入力してください")]
         [Range(0, 999999, ErrorMessage = "{0}は数値で入力してください")]
         public int OrderNo { get; set; }
         public int DetailNo { get; set; }
-        [DisplayFormat(DataFormatString = "0:d3")]
+        [DisplayFormat(DataFormatString = "{0:d3}")]
         public int MeisaiNo { get; set; }
         [DisplayName("注文番号-明細")]
         public string OrderDetail { get; set; }
@@ -29,7 +29,7 @@ namespace OrderingManagementSystem.ViewModel
         [DisplayName("商品名")]
         public string ItemName { get; set; }
         [DisplayName("数量")]
-        [DisplayFormat(DataFormatString = "n*")]
+        [DisplayFormat(DataFormatString = "{0}")]
         public decimal Quantity { get; set; }
         [DisplayName("希望納期")]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
